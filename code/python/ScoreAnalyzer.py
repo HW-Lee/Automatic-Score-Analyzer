f4032b5febe76eb8e21cf51fbd613acc4ec744f6
HW-Lee <tony123930@yahoo.com.tw>
2016-01-04 11:44:12 +0800

    [SA] find staffline centers

--- code/python/ScoreAnalyzer.py
+++ code/python/ScoreAnalyzer.py
@@ -23,6 +23,7 @@ def _binarize_image(raw_image=np.array([]), thresh=.5, dtype=float):
 
 def _run_length_coding(array_data=np.array([])):
     if sum(array_data.shape) > 0:
+        
         switch_pts = [i for i, x in enumerate(map(lambda x, y: x != y, array_data[:-1], array_data[1:])) if x]
         switch_pts.insert(0, -1)
         switch_pts.append(len(array_data)-1)
@@ -30,6 +31,7 @@ def _run_length_coding(array_data=np.array([])):
         run_length_sequence = np.diff(switch_pts)
         sequence_id = (np.arange(len(run_length_sequence)) + array_data[0]) % 2
 
+        # Each element consists of two values: (SET, RUN_LENGTH)
         return zip(sequence_id, run_length_sequence)
     else:
         return None
@@ -52,6 +54,54 @@ def _run_length_distribution(data):
 
     return hist
 
+def _find_staffline_centers(col, width, space):
+    # Check for existence of staffline
+    # Simplest rule: the length must be greater than 9 (for 5 lines and 4 spaces)
+    if len(col) < 9: return []
+
+    col = np.array(col, dtype=int)
+    acc_offset = np.zeros(col.shape, dtype=int)
+    for x in range(1, len(acc_offset)):
+        acc_offset[x] = acc_offset[x-1] + col[x-1]
+
+    # Generatetesting frames
+    #                               center
+    #                                 |
+    #                                 v
+    # [ width, space, width, space, width, space, width, space, width ]
+    # [ rl[0], rl[1], rl[2], rl[3], rl[4], rl[5], rl[6], rl[7], rl[8] ]
+    # [ rl[1], rl[2], rl[3], rl[4], rl[5], rl[6], rl[7], rl[8], rl[9] ]
+    # ...
+    candidates = [
+        col[0:-8],
+        col[1:-7],
+        col[2:-6],
+        col[3:-5],
+        col[4:-4],
+        col[5:-3],
+        col[6:-2],
+        col[7:-1],
+        col[8:]
+    ]
+    candidates = np.array(candidates).transpose()
+
+    # Select the indices of possible stafflines centers
+    results = map(_is_staffline, candidates, [width]*len(candidates), [space]*len(candidates))
+    center_idx = np.array([i for i, x in enumerate(results) if x], dtype=int) + 4
+
+    return acc_offset[center_idx] + width/2
+
+def _is_staffline(arr=np.array([]), width=0, space=0, err_thresh=.5):
+    if len(arr) == 9:
+        height = 5*width + 4*space
+        if abs(arr[0] - width) / float(width) > err_thresh: return False
+        perfect_staffline = np.array([width, space, width, space, width, space, width, space, width], dtype=float)
+        diff = abs(np.array(arr) - perfect_staffline)
+        diff_rat = np.divide(diff, perfect_staffline)
+
+        return reduce(lambda x, y: x and y, diff_rat < err_thresh)
+    else:
+        return None
 
 def staffline_info(data):
     # RLC at each column
