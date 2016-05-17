365cba5279297ddf69b9dc83244c4db286527fa4
HW-Lee <tony123930@yahoo.com.tw>
2016-05-17 19:18:06 +0800

    [SA] update notehead detector model

--- code/python/ScoreAnalyzer/detector/restdetector.py
+++ code/python/ScoreAnalyzer/detector/restdetector.py
@@ -1,27 +1,29 @@
 import os
 import glob
 import numpy as np
+from ScoreAnalyzer.detector.utils import find_region
 from ScoreAnalyzer.image import imagetools as it
 from scipy import misc
 
 rootpath = os.path.dirname(os.path.abspath(__file__))
-SIM_THRESH = {
-    1 : .75,
-    2 : .75,
-    4 : .75,
-    8 : .65,
-    16: .7,
-    32: .7,
-    64: .7
-}
 
 class RestClassifier(object):
+    SIM_THRESH = {
+        1 : .7,
+        2 : .7,
+        4 : .75,
+        8 : .65,
+        16: .7,
+        32: .7,
+        64: .7
+    }
+
     def __init__(self):
         imglist = glob.glob("{}/models/rest*.png".format(rootpath))
         imgid = map(lambda x: int(x.split("/")[-1].replace("rest", "").replace(".png", "")), imglist)
         tpl_imgs = map(lambda x: it.load(x, reverse=True), imglist)
         self.templates = dict(zip(imgid, tpl_imgs))
-        self.decision_thresh = SIM_THRESH
+        self.decision_thresh = RestClassifier.SIM_THRESH
 
     def _cos_similarity(self, img, template):
         img = np.array(img)
@@ -49,7 +51,10 @@ class RestClassifier(object):
         feats = np.array(feats)
         template = template.flatten()
         template /= np.linalg.norm(template, ord=2)
-        return np.vstack([np.dot(feats, template), np.arange(num_frames)+dp_offset]).T
+
+        starts = np.arange(num_frames)+dp_offset
+        ends = starts + len(template)/img.shape[0]
+        return np.vstack([np.dot(feats, template), starts, ends]).T
 
     def _eval_conf(self, img):
         keys, templates = zip(*self.templates.items())
@@ -63,16 +68,18 @@ class RestClassifier(object):
         conflist = map(self._eval_conf, imgs)
         return conflist if len(imgs) > 1 else conflist[0]
 
-    def _find_best_matched(self, img):
+    def _find_matched(self, img, eliminate_empty):
         confs = self._eval_conf(img)
         for key, mat in confs.items():
-            confs[key] = mat[np.argmax(mat[:, 0]), :]
+            rs = find_region(mat[:, 0] > self.decision_thresh[key], find=1, merge_thresh=0)
+            confs[key] = np.array(map(lambda r: mat[r[0]+np.argmax(mat[r[0]:r[1]+1, 0]), :], rs))
+            if eliminate_empty and np.sum(confs[key].shape) == 0: confs.pop(key, None)
 
-        return np.array(map(lambda k: np.hstack([k, confs[k]]), sorted(confs.keys())))
+        return confs
 
-    def find_best_matched(self, imgs):
+    def find_matched(self, imgs, eliminate_empty=False):
         if not isinstance(imgs, list): imgs = [imgs]
         if len(imgs) == 0: return []
 
-        bmtched_list = map(self._find_best_matched, imgs)
+        bmtched_list = map(lambda img: self._find_matched(img, eliminate_empty), imgs)
         return bmtched_list if len(imgs) > 1 else bmtched_list[0]
