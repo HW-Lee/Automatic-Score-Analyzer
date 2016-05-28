d718d1420d688757d5f6fd74ba31764ba65fe00c
HW-Lee <tony123930@yahoo.com.tw>
2016-05-28 17:24:22 +0800

    [SA] nhdetector: type classification

--- code/python/ScoreAnalyzer/detector/nhdetector.py
+++ code/python/ScoreAnalyzer/detector/nhdetector.py
@@ -8,6 +8,14 @@ rootpath = os.path.dirname(os.path.abspath(__file__))
 
 class NoteHeadFeatureExtractor(object):
     @staticmethod
+    def get_tiny_image(img, size):
+        img = np.array(img)
+        aspect_ratio = img.shape[1] * 1. / img.shape[0]
+        ref_aspect_ratio = size[1] * 1. / size[0]
+
+        return misc.imresize(img, size) * np.min([1., aspect_ratio / ref_aspect_ratio]) / 255. * 2 - 1
+
+    @staticmethod
     def get_feat(img, width, tighten=False):
         img = np.array(img)
         feat = []
@@ -26,6 +34,10 @@ class NoteHeadFeatureExtractor(object):
         return feat
 
     @staticmethod
+    def find_feat_peak(img, width, tighten=False):
+        return 1 - abs(width-1-np.argmax(NoteHeadFeatureExtractor.get_feat(img, width)))*1./(width-1)
+
+    @staticmethod
     def _binimg_tighten(img):
         img = np.array(img)
         vert = np.sum(img, axis=0) > 0
@@ -42,8 +54,9 @@ class NoteHeadFeatureExtractor(object):
         return np.array([n, num-n]).transpose()
 
 class NoteHeadDetector(object):
-    def __init__(self, model="notehead20160517-1", decision_thresh=0):
+    def __init__(self, model="notehead20160517-1", typemodel="notehead_clf20160528", decision_thresh=0):
         self.clf = joblib.load("/".join([rootpath, "models", model]) + ".pkl")
+        self.typeclf = joblib.load("/".join([rootpath, "models", typemodel]) + ".pkl")
         self.decision_thresh = decision_thresh
         self.featwidth = 12
 
@@ -55,3 +68,7 @@ class NoteHeadDetector(object):
         if img.shape[0]/1.5 > img.shape[1] or img.shape[1]/1.5 > img.shape[0]: return -1
         feat = NoteHeadFeatureExtractor.get_feat(img=img, width=self.featwidth, tighten=tighten)
         return self.clf.decision_function([feat])[0][0]
+
+    def get_type(self, img, tighten=False):
+        if self.typeclf.predict([NoteHeadFeatureExtractor.get_tiny_image(img, [12, 16]).flatten()])[0] == 0: return "nh-h"
+        else: return "nh-s"
